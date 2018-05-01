0b052e86803a5453b6bf08819a201183c218f8eb
kikema <kikemartinez1996@gmail.com>
2018-05-01 18:28:24 +0200

    Mensaje mavlink de prueba

--- src/examples/px4_simple_app/px4_simple_app.cpp
+++ src/examples/px4_simple_app/px4_simple_app.cpp
@@ -66,6 +66,7 @@ extern "C" __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
 class SensorDistancia;
 
+#define BUFFER_LENGTH 2041 // minimum buffer size that can be used with qnx (I don't know why)
 
 namespace sensor_distancia_q
 {
@@ -159,6 +160,7 @@ void SensorDistancia::task_main_sensor_distancia(int argc, char *argv[])
 void SensorDistancia::task_main()
 {
 	PX4_INFO("Hello Sky!");
+	PX4_INFO("1");
 
 	int sensor_sub_fd_sensor = orb_subscribe(ORB_ID(distance_sensor));
 	int vehicle_control_mode_fd = orb_subscribe(ORB_ID(vehicle_control_mode));
@@ -167,198 +169,300 @@ void SensorDistancia::task_main()
 	orb_set_interval(sensor_sub_fd_sensor, 200);
 	orb_set_interval(vehicle_control_mode_fd, 200);
 	orb_set_interval(vehicle_status_fd, 200);
+	PX4_INFO("2");
 
+
+	char target_ip[100];
+
+	int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	struct sockaddr_in gcAddr;
+	struct sockaddr_in locAddr;
+	//struct sockaddr_in fromAddr;
+	uint8_t buf[BUFFER_LENGTH];
+	ssize_t recsize;
+	socklen_t fromlen;
+	mavlink_message_t msg;
+	uint16_t len;
+	int i = 0;
+	//int success = 0;
+	unsigned int temp = 0;
+
+	PX4_INFO("3");
+
+	strcpy(target_ip, "127.0.0.1");
+	memset(&locAddr, 0, sizeof(locAddr));
+	locAddr.sin_family = AF_INET;
+	locAddr.sin_addr.s_addr = INADDR_ANY;
+	locAddr.sin_port = htons(14551);
+	if (-1 == bind(sock,(struct sockaddr *)&locAddr, sizeof(struct sockaddr))){
+		PX4_INFO("Error Bind");
+		return;
+	}
+
+	PX4_INFO("4");
+
+	memset(&gcAddr, 0, sizeof(gcAddr));
+	gcAddr.sin_family = AF_INET;
+	gcAddr.sin_addr.s_addr = inet_addr(target_ip);
+	gcAddr.sin_port = htons(14550);
+
+
+	for (;;)
+	{
+		PX4_INFO("5");
+
+		/* Send Status
+		   uint32_t onboard_control_sensors_present,
+			  uint32_t onboard_control_sensors_enabled,
+				uint32_t onboard_control_sensors_health,
+				uint16_t load,
+				uint16_t voltage_battery,
+				 int16_t current_battery,
+				 int8_t battery_remaining,
+				  uint16_t drop_rate_comm,
+					 uint16_t errors_comm,
+					 uint16_t errors_count1,
+					  uint16_t errors_count2,
+						 uint16_t errors_count3,
+						 uint16_t errors_count4)
+ */
+		mavlink_msg_sys_status_pack(1, 1, &msg, 0, 0, 0, 500, 11000, -1, -1, 0, 0, 0, 0, 0, 0);
+		PX4_INFO("5.1");
+
+		len = mavlink_msg_to_send_buffer(buf, &msg);
+
+		PX4_INFO("5.5");
+
+		//int bytes_sent = sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof (struct sockaddr_in));
+		sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof (struct sockaddr_in));
+
+		memset(buf, 0, BUFFER_LENGTH);
+		recsize = recvfrom(sock, (void *)buf, BUFFER_LENGTH, 0, (struct sockaddr *)&gcAddr, &fromlen);
+		if (recsize > 0){
+			// Something received - print out all bytes and parse packet
+
+			PX4_INFO("6");
+
+			mavlink_message_t msg1;
+			msg1.sysid = 0;
+			msg1.compid = 0;
+			msg1.len = 0;
+			msg1.msgid = 0;
+
+			mavlink_status_t status;
+
+			printf("Bytes Received: %d\nDatagram: ", (int)recsize);
+
+			PX4_INFO("7");
+
+			for (i = 0; i < recsize; ++i)
+			{
+				temp = buf[i];
+				printf("%02x ", (unsigned char)temp);
+				if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg1, &status))
+				{
+					// Packet received
+					printf("\nReceived packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\n", msg1.sysid, msg1.compid, msg1.len, msg1.msgid);
+				}
+			}
+			printf("\n");
+		}
+		memset(buf, 0, BUFFER_LENGTH);
+		sleep(1); // Sleep one second
+	}
+
+	/*
 
 	while(!_task_should_exit){
-		struct distance_sensor_s distance_sensor_param;
-		struct vehicle_control_mode_s vehicle_control_mode_param;
-		struct vehicle_status_s status;
+	struct distance_sensor_s distance_sensor_param;
+	struct vehicle_control_mode_s vehicle_control_mode_param;
+	struct vehicle_status_s status;
 
-		orb_copy(ORB_ID(distance_sensor), sensor_sub_fd_sensor, &distance_sensor_param);
-		orb_copy(ORB_ID(vehicle_control_mode), vehicle_control_mode_fd, &vehicle_control_mode_param);
-		orb_copy(ORB_ID(vehicle_status), vehicle_status_fd, &status);
-
-
-
-		if (0.5f < distance_sensor_param.current_distance){
-			/*
-			PX4_INFO("distance_sensor:\n\tmin_distance\t%8.4f\n\tmax_distance\t%8.4f\n\tcurrent_distance\t%8.4f\n\tcovariance\t%8.4f\n\ttype\t%d\n\tOrientation\t%d\n\n",
-			(double)distance_sensor_param.min_distance,
-			(double)distance_sensor_param.max_distance,
-			(double)distance_sensor_param.current_distance,
-			(double)distance_sensor_param.covariance,
-			(uint8_t)distance_sensor_param.type,
-			(uint16_t)distance_sensor_param.orientation);
-
-			PX4_INFO("vehicle_control_mode:\n\tflag_armed\t%d\n\tflag_control_auto_enabled\t%d\n\tflag_control_manual_enabled\t%d\n\n",
-			vehicle_control_mode_param.flag_armed,
-			vehicle_control_mode_param.flag_control_auto_enabled,
-			vehicle_control_mode_param.flag_control_manual_enabled
-		);
-		*/
-		if ( distance_sensor_param.current_distance < 10 && auxKike == 1){
-			/*
-			PX4_INFO("vehicle_control_mode:\n\tflag_armed\t%d\n\tflag_control_auto_enabled\t%d\n\tflag_control_manual_enabled\t%d\n\n",
-			vehicle_control_mode_param.flag_armed,
-			vehicle_control_mode_param.flag_control_auto_enabled,
-			vehicle_control_mode_param.flag_control_manual_enabled
-		);
-		*/
+	orb_copy(ORB_ID(distance_sensor), sensor_sub_fd_sensor, &distance_sensor_param);
+	orb_copy(ORB_ID(vehicle_control_mode), vehicle_control_mode_fd, &vehicle_control_mode_param);
+	orb_copy(ORB_ID(vehicle_status), vehicle_status_fd, &status);
 
 
 
-/*
-		// Con este comando pausamos la misión y para que no se choque.
-
-		struct vehicle_command_s cmd = {
-			.timestamp = 0,
-			.param5 = NAN,
-			.param6 = NAN,
-			.param1 = 1,
-			.param2 = 4,
-			.param3 = 3,
-			.param4 = NAN,
-			.param7 = NAN,
-			.command = vehicle_command_s::VEHICLE_CMD_DO_SET_MODE,
-			.target_system = status.system_id,
-			.target_component = status.component_id,
-			.source_system = 1,
-			.source_component = 2,
-			.confirmation = 1,
-			.from_external = true
-		};
-		orb_advert_t h = orb_advertise_queue(ORB_ID(vehicle_command), &cmd, vehicle_command_s::ORB_QUEUE_LENGTH);
-		(void)orb_unadvertise(h);
-*/
-
-	const struct vehicle_command_s cmd {
-		.timestamp = 0,
-		.param1 = 0,
-		.param2 = 1,
-		.param3 = 0,
-		.param4 = NAN,
-		.param5 = 47.396675f,
-		.param6 = 8.550455f,
-		.param7 = 10.000000f,
-		.command = MAV_CMD_NAV_WAYPOINT,
-		.target_system = status.system_id,
-		.target_component = status.component_id,
-		.source_system = 1,
-		.source_component = 2,
-		.confirmation = 1,
-		.from_external = true
-	};
-
-	MavlinkCommandSender::instance().handle_vehicle_command(cmd, mavlink_channel_t(MAVLINK_COMM_0) );
-/*
-	const mavlink_mission_count_t wpc {
-		.count = 1,
-		.target_system = 1,
-		.target_component = 190,
-		.mission_type = 0
-	};
-
-		//Creo el mensaje con el waypoint
-		const mavlink_mission_item_t wp {
-			.param1 = 0.0f,
-			.param2 = 0.0f,
-			.param3 = 0.0f,
-			.param4 = NAN,
-			.x = 47.396675f,
-			.y = 8.550455f,
-			.z = 10.000000f,
-			.seq = 0,
-			.command = 22,
-			.target_system = 1,
-			.target_component = 190,
-			.frame = 3,
-			.current = 1,
-			.autocontinue = 1,
-			.mission_type = 0
-		};
-
-		// Imprimo mensaje creado anteriomente
-		PX4_INFO("El mensaje creado es: \n\t	param1 %f,\n\t	param2 %f,\n\t	param3 %f,\n\t	param4 %f,\n\t	x %f,\n\t	y %f,\n\t	z %f,\n\t	seq %d,\n\t	command %d,\n\t	target_system %d,\n\t	target_component %d,\n\t	frame %d,\n\t	current %d,\n\t	autocontinue %d,\n\t	mission_type %d,\n", wp.param1,
-		wp.param2,
-		wp.param3,
-		wp.param4,
-		wp.x,
-		wp.y,
-		wp.z,
-		wp.seq,
-		wp.command,
-		wp.target_system,
-		wp.target_component,
-		wp.frame,
-		wp.current,
-		wp.autocontinue,
-		wp.mission_type);
-
-		mavlink_message_t msgc;
-		mavlink_message_t msg;
-
-		PX4_INFO("El mensaje cr1");
-
-		Mavlink * mavlink = new Mavlink();
-
-		PX4_INFO("El mensaje cr2");
-
-		uint8_t system_id = mavlink->get_system_id();
-		uint8_t component_id = mavlink->get_component_id();
-
-		PX4_INFO("El mensaje cr3");
-
-		// Genero mensaje como si enviara desde mavlink
-		// uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_mission_item_t* mission_item)
-		mavlink_msg_mission_item_encode(system_id, component_id, &msg, &wp);
-		mavlink_msg_mission_count_encode(255, 0, &msgc, &wpc);
-
-		PX4_INFO("El mensaje cr4");
-
-		// Llamo al manejador de mavlink_mission
-		MavlinkMissionManager *_mission_manager = new MavlinkMissionManager(mavlink);
-		PX4_INFO("El mensaje cr5");
-
-		_mission_manager->handle_message(&msgc);
-
-		PX4_INFO("El mensaje cr6");
-
-		//_mission_manager->handle_message(&msg);
-
-
-		PX4_INFO("El mensaje cr");
-*/
-
-		/*
-		// Entro dentro de state posctl pero necesito
-		cmd = {
-		.timestamp = 0,
-		.param5 = NAN,
-		.param6 = NAN,
-		.param1 = 1,
-		.param2 = 3,
-		.param3 = 0,
-		.param4 = NAN,
-		.param7 = NAN,
-		.command = vehicle_command_s::VEHICLE_CMD_DO_SET_MODE,
-		.target_system = status.system_id,
-		.target_component = status.component_id,
-		.source_system = 1,
-		.source_component = 2,
-		.confirmation = 1,
-		.from_external = true
-	};
-	*/
-	//	h = orb_advertise_queue(ORB_ID(vehicle_command), &cmd, vehicle_command_s::ORB_QUEUE_LENGTH);
-	//	(void)orb_unadvertise(h);
-
+	if (0.5f < distance_sensor_param.current_distance){
+	*//*
+	PX4_INFO("distance_sensor:\n\tmin_distance\t%8.4f\n\tmax_distance\t%8.4f\n\tcurrent_distance\t%8.4f\n\tcovariance\t%8.4f\n\ttype\t%d\n\tOrientation\t%d\n\n",
+	(double)distance_sensor_param.min_distance,
+	(double)distance_sensor_param.max_distance,
+	(double)distance_sensor_param.current_distance,
+	(double)distance_sensor_param.covariance,
+	(uint8_t)distance_sensor_param.type,
+	(uint16_t)distance_sensor_param.orientation);
 
 	PX4_INFO("vehicle_control_mode:\n\tflag_armed\t%d\n\tflag_control_auto_enabled\t%d\n\tflag_control_manual_enabled\t%d\n\n",
 	vehicle_control_mode_param.flag_armed,
 	vehicle_control_mode_param.flag_control_auto_enabled,
-	auxKike
+	vehicle_control_mode_param.flag_control_manual_enabled
 );
-auxKike = 0;
+*/
+//	if ( distance_sensor_param.current_distance < 10 && auxKike == 1){
+/*
+PX4_INFO("vehicle_control_mode:\n\tflag_armed\t%d\n\tflag_control_auto_enabled\t%d\n\tflag_control_manual_enabled\t%d\n\n",
+vehicle_control_mode_param.flag_armed,
+vehicle_control_mode_param.flag_control_auto_enabled,
+vehicle_control_mode_param.flag_control_manual_enabled
+);
+*/
+
+
+/*
+// Con este comando pausamos la misión y para que no se choque.
+
+struct vehicle_command_s cmd = {
+.timestamp = 0,
+.param5 = NAN,
+.param6 = NAN,
+.param1 = 1,
+.param2 = 4,
+.param3 = 3,
+.param4 = NAN,
+.param7 = NAN,
+.command = vehicle_command_s::VEHICLE_CMD_DO_SET_MODE,
+.target_system = status.system_id,
+.target_component = status.component_id,
+.source_system = 1,
+.source_component = 2,
+.confirmation = 1,
+.from_external = true
+};
+orb_advert_t h = orb_advertise_queue(ORB_ID(vehicle_command), &cmd, vehicle_command_s::ORB_QUEUE_LENGTH);
+(void)orb_unadvertise(h);
+*/
+/*
+const struct vehicle_command_s cmd {
+.timestamp = 0,
+.param1 = 0,
+.param2 = 1,
+.param3 = 0,
+.param4 = NAN,
+.param5 = 47.396675f,
+.param6 = 8.550455f,
+.param7 = 10.000000f,
+.command = MAV_CMD_NAV_WAYPOINT,
+.target_system = status.system_id,
+.target_component = status.component_id,
+.source_system = 1,
+.source_component = 2,
+.confirmation = 1,
+.from_external = true
+};
+
+MavlinkCommandSender::instance().handle_vehicle_command(cmd, mavlink_channel_t(MAVLINK_COMM_0) );
+*/
+/*
+const mavlink_mission_count_t wpc {
+.count = 1,
+.target_system = 1,
+.target_component = 190,
+.mission_type = 0
+};
+
+//Creo el mensaje con el waypoint
+const mavlink_mission_item_t wp {
+.param1 = 0.0f,
+.param2 = 0.0f,
+.param3 = 0.0f,
+.param4 = NAN,
+.x = 47.396675f,
+.y = 8.550455f,
+.z = 10.000000f,
+.seq = 0,
+.command = 22,
+.target_system = 1,
+.target_component = 190,
+.frame = 3,
+.current = 1,
+.autocontinue = 1,
+.mission_type = 0
+};
+
+// Imprimo mensaje creado anteriomente
+PX4_INFO("El mensaje creado es: \n\t	param1 %f,\n\t	param2 %f,\n\t	param3 %f,\n\t	param4 %f,\n\t	x %f,\n\t	y %f,\n\t	z %f,\n\t	seq %d,\n\t	command %d,\n\t	target_system %d,\n\t	target_component %d,\n\t	frame %d,\n\t	current %d,\n\t	autocontinue %d,\n\t	mission_type %d,\n", wp.param1,
+wp.param2,
+wp.param3,
+wp.param4,
+wp.x,
+wp.y,
+wp.z,
+wp.seq,
+wp.command,
+wp.target_system,
+wp.target_component,
+wp.frame,
+wp.current,
+wp.autocontinue,
+wp.mission_type);
+
+mavlink_message_t msgc;
+mavlink_message_t msg;
+
+PX4_INFO("El mensaje cr1");
+
+Mavlink * mavlink = new Mavlink();
+
+PX4_INFO("El mensaje cr2");
+
+uint8_t system_id = mavlink->get_system_id();
+uint8_t component_id = mavlink->get_component_id();
+
+PX4_INFO("El mensaje cr3");
+
+// Genero mensaje como si enviara desde mavlink
+// uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_mission_item_t* mission_item)
+mavlink_msg_mission_item_encode(system_id, component_id, &msg, &wp);
+mavlink_msg_mission_count_encode(255, 0, &msgc, &wpc);
+
+PX4_INFO("El mensaje cr4");
+
+// Llamo al manejador de mavlink_mission
+MavlinkMissionManager *_mission_manager = new MavlinkMissionManager(mavlink);
+PX4_INFO("El mensaje cr5");
+
+_mission_manager->handle_message(&msgc);
+
+PX4_INFO("El mensaje cr6");
+
+//_mission_manager->handle_message(&msg);
+
+
+PX4_INFO("El mensaje cr");
+*/
+
+/*
+// Entro dentro de state posctl pero necesito
+cmd = {
+.timestamp = 0,
+.param5 = NAN,
+.param6 = NAN,
+.param1 = 1,
+.param2 = 3,
+.param3 = 0,
+.param4 = NAN,
+.param7 = NAN,
+.command = vehicle_command_s::VEHICLE_CMD_DO_SET_MODE,
+.target_system = status.system_id,
+.target_component = status.component_id,
+.source_system = 1,
+.source_component = 2,
+.confirmation = 1,
+.from_external = true
+};
+*/
+//	h = orb_advertise_queue(ORB_ID(vehicle_command), &cmd, vehicle_command_s::ORB_QUEUE_LENGTH);
+//	(void)orb_unadvertise(h);
+
+/*
+PX4_INFO("vehicle_control_mode:\n\tflag_armed\t%d\n\tflag_control_auto_enabled\t%d\n\tflag_control_manual_enabled\t%d\n\n",
+vehicle_control_mode_param.flag_armed,
+vehicle_control_mode_param.flag_control_auto_enabled,
+auxKike
+);
+auxKike = 0;*/
 /*
 struct vehicle_command_s cmd = {
 .timestamp = 0,
@@ -381,11 +485,11 @@ struct vehicle_command_s cmd = {
 orb_advert_t h = orb_advertise_queue(ORB_ID(vehicle_command), &cmd, vehicle_command_s::ORB_QUEUE_LENGTH);
 (void)orb_unadvertise(h);
 
-*/
+*//*
 }
 }
 
-}
+}*/
 PX4_INFO("GoodBye");
 }
 
